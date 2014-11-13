7d3c07cdbbccd9caa30b0bc03a8ab80dc9bca2ef
Zheng SHAO <axot@msn.com>
2014-11-14 01:37:52 +0900

    Update viewer.php
    add oc7 support

--- files_pdfmime/ajax/viewer.php
+++ files_pdfmime/ajax/viewer.php
@@ -4,21 +4,45 @@
 OCP\User::checkLoggedIn();
 
 $filename = $_GET["dir"] . "/" . $_GET["file"];
+$ver = OCP\Util::getVersion();
+$major = (int)$ver[0];
 
-if(!OC_Filesystem::file_exists($filename)) {
-	header("HTTP/1.0 404 Not Found");
-	$tmpl = new OCP\Template( '', '404', 'guest' );
-	$tmpl->assign('file',$filename);
-	$tmpl->printPage();
-	exit;
+if($major < 7)
+{
+    if(!OC_Filesystem::file_exists($filename)) {
+        header("HTTP/1.0 404 Not Found");
+        $tmpl = new OCP\Template( '', '404', 'guest' );
+        $tmpl->assign('file',$filename);
+        $tmpl->printPage();
+        exit;
+    }
+
+    $ftype=OC_Filesystem::getMimeType( $filename );
+
+    header('Content-Type:'.$ftype);
+
+    OCP\Response::disableCaching();
+    header('Content-Length: '.OC_Filesystem::filesize($filename));
+
+    OC_Util::obEnd();
+    OC_Filesystem::readfile( $filename );
 }
+else{
+    if(!\OC\Files\Filesystem::file_exists($filename)) {
+        header("HTTP/1.0 404 Not Found");
+        $tmpl = new OCP\Template( '', '404', 'guest' );
+        $tmpl->assign('file',$filename);
+        $tmpl->printPage();
+        exit;
+    }
 
-$ftype=OC_Filesystem::getMimeType( $filename );
+    $ftype=\OC\Files\Filesystem::getMimeType( $filename );
 
-header('Content-Type:'.$ftype);
+    header('Content-Type:'.$ftype);
 
-OCP\Response::disableCaching();
-header('Content-Length: '.OC_Filesystem::filesize($filename));
+    OCP\Response::disableCaching();
+    header('Content-Length: '.\OC\Files\Filesystem::filesize($filename));
 
-OC_Util::obEnd();
-OC_Filesystem::readfile( $filename );
+    OC_Util::obEnd();
+    \OC\Files\Filesystem::readfile( $filename );
+}
